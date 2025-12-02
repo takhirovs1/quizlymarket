0ec4d4cd49a5a35693e5438b82a830e8cbb7cff4
muroderkinov <muroderkinov@udevs.io>
2025-12-02 13:08:21 +0500

    Increment app version to 1.0.0+9 in pubspec.yaml and update corresponding version references in generated Dart file. This change reflects the latest build and ensures consistency across the project.

--- lib/src/common/constant/pubspec.yaml.g.dart
+++ lib/src/common/constant/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'1.0.0+8',
+    representation: r'1.0.0+9',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'1.0.0+8',
+    canonical: r'1.0.0+9',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,7 +117,7 @@ sealed class Pubspec {
     preRelease: <String>[],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'8'],
+    build: <String>[r'9'],
   );
 
   /// Build date and time (UTC)
@@ -125,11 +125,11 @@ sealed class Pubspec {
     2025,
     12,
     2,
-    7,
-    2,
-    5,
-    84,
-    827,
+    8,
+    6,
+    19,
+    23,
+    873,
   );
 
   /// Name
