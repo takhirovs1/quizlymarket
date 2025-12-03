43895415609f33a54446e62f3ef9c5139d99a5e5
muroderkinov <muroderkinov@udevs.io>
2025-12-03 15:57:30 +0500

    Increment app version to 1.0.0+39 and update generated files
    - Updated the app version in pubspec.yaml and corresponding generated files to 1.0.0+39.
    - Adjusted version representation in pubspec.yaml.g.dart to reflect the new version.
    - Ensured all related files are consistent with the updated version for proper application deployment.

--- lib/src/common/constant/pubspec.yaml.g.dart
+++ lib/src/common/constant/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'1.0.0+38',
+    representation: r'1.0.0+39',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'1.0.0+38',
+    canonical: r'1.0.0+39',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,7 +117,7 @@ sealed class Pubspec {
     preRelease: <String>[],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'38'],
+    build: <String>[r'39'],
   );
 
   /// Build date and time (UTC)
@@ -126,10 +126,10 @@ sealed class Pubspec {
     12,
     3,
     10,
-    38,
-    38,
-    54,
-    526,
+    56,
+    2,
+    940,
+    931,
   );
 
   /// Name
