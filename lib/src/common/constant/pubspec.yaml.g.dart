d87e8ea510293ac30b2834d9b59136911aacb1e7
Abrorjon755 <Abrorjonbahodirov@gmail.com>
2025-12-03 17:25:03 +0500

    Increment app version to 1.0.0+45 and update related constants
    - Updated the app version in pubspec.yaml from 1.0.0+44 to 1.0.0+45.
    - Adjusted version representation in generated Dart file to reflect the new version.
    - Ensured all related constants are consistent with the updated version.

--- lib/src/common/constant/pubspec.yaml.g.dart
+++ lib/src/common/constant/pubspec.yaml.g.dart
@@ -92,13 +92,13 @@ sealed class Pubspec {
   static const PubspecVersion version = (
     /// Non-canonical string representation of the version as provided
     /// in the pubspec.yaml file.
-    representation: r'1.0.0+44',
+    representation: r'1.0.0+45',
 
     /// Returns a 'canonicalized' representation
     /// of the application version.
     /// This represents the version string in accordance with
     /// Semantic Versioning (SemVer) standards.
-    canonical: r'1.0.0+44',
+    canonical: r'1.0.0+45',
 
     /// MAJOR version when you make incompatible API changes.
     /// The major version number: 1 in "1.2.3".
@@ -117,7 +117,7 @@ sealed class Pubspec {
     preRelease: <String>[],
 
     /// The build identifier: "foo" in "1.2.3+foo".
-    build: <String>[r'44'],
+    build: <String>[r'45'],
   );
 
   /// Build date and time (UTC)
@@ -126,10 +126,10 @@ sealed class Pubspec {
     12,
     3,
     12,
-    5,
-    52,
-    625,
-    516,
+    24,
+    8,
+    171,
+    317,
   );
 
   /// Name
